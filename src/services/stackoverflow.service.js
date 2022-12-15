f5ea067efaa63d33523c647ccb940c873df780c0
Bongshim <bongshime@gmail.com>
2022-12-15 21:47:59 +0100

    added try catch blocks to api calls

--- src/services/stackoverflow.service.js
+++ src/services/stackoverflow.service.js
@@ -10,42 +10,47 @@ const logger = require('../config/logger');
  */
 const getStackOverflowAnswers = async (links) => {
   const answersBody = [];
+  try {
+    await Promise.all(
+      links.map(async (item) => {
+        const pack = {};
+        const answerPage = `https://stackoverflow.com/a/${item}`;
+        const res = await axiosCall.get(answerPage);
+        let $ = cheerio.load(res.data);
+        pack.question = $('.fs-headline1').children().html();
+        const content = $(`#answer-${item}`).html();
+        $ = cheerio.load(content);
+        pack.answer = $('.js-post-body').html();
+        pack.stackOverflow = true;
+        answersBody.push(pack);
+      })
+    );
 
-  await Promise.all(
-    links.map(async (item) => {
-      const pack = {};
-      const answerPage = `https://stackoverflow.com/a/${item}`;
-      const res = await axiosCall.get(answerPage);
-      let $ = cheerio.load(res.data);
-      pack.question = $('.fs-headline1').children().html();
-      const content = $(`#answer-${item}`).html();
-      $ = cheerio.load(content);
-      pack.answer = $('.js-post-body').html();
-      pack.stackOverflow = true;
-      answersBody.push(pack);
-    })
-  );
-
-  return answersBody;
+    return answersBody;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getStackOverflowAnswerIds = async (questionIds) => {
   const answerIds = [];
+  try {
+    await Promise.all(
+      questionIds.map(async (id) => {
+        const response = await axiosCall.get(
+          `https://api.stackexchange.com/2.3/questions/${id}/answers?order=desc&sort=activity&site=stackoverflow`
+        );
+        const eachId = response.data.items;
 
-  await Promise.all(
-    questionIds.map(async (id) => {
-      const response = await axiosCall.get(
-        `https://api.stackexchange.com/2.3/questions/${id}/answers?order=desc&sort=activity&site=stackoverflow`
-      );
-      const eachId = response.data.items;
-
-      eachId.map(async (item) => {
-        answerIds.push(item.answer_id);
-      });
-    })
-  );
-
-  return answerIds;
+        eachId.map(async (item) => {
+          answerIds.push(item.answer_id);
+        });
+      })
+    );
+    return answerIds;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 /**
@@ -54,14 +59,18 @@ const getStackOverflowAnswerIds = async (questionIds) => {
  * @return {promise<result>}
  */
 const searchStackOverflow = async (string) => {
-  const response = await axiosCall.get(`/2.3/search/excerpts?order=desc&sort=votes&body=${string}&site=stackoverflow`);
-  const result = response.data.items;
-  const filtered = result.filter((result) => result?.answer_count > 0);
-  const questionIds = filtered.map((item) => item.question_id);
-  const answerIds = await getStackOverflowAnswerIds(questionIds);
-  const answers = await getStackOverflowAnswers(answerIds);
+  try {
+    const response = await axiosCall.get(`/2.3/search/excerpts?order=desc&sort=votes&body=${string}&site=stackoverflow`);
+    const result = response.data.items;
+    const filtered = result.filter((result) => result?.answer_count > 0);
+    const questionIds = filtered.map((item) => item.question_id);
+    const answerIds = await getStackOverflowAnswerIds(questionIds);
+    const answers = await getStackOverflowAnswers(answerIds);
 
-  return answers;
+    return answers;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = { searchStackOverflow };
